24ec6aecab2ae7d08ab729c7a343465d1ec3af90
Luis Vasquez <vluis5457@gmail.com>
2024-02-18 22:06:44 -0600

    Mostrando link para abrir el swagger

--- src/main/java/org/example/inventario/InventarioApplication.java
+++ src/main/java/org/example/inventario/InventarioApplication.java
@@ -1,13 +1,25 @@
 package org.example.inventario;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @SpringBootApplication
-public class InventarioApplication {
+public class InventarioApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(InventarioApplication.class, args);
     }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Swagger UI: http://localhost:8080/swagger-ui/index.html#/");
+    }
+
 
 }
